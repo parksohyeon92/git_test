5cb165ecf868dd31a37c29b35ff59aec2761ec81
park.sohyun01 <park.sohyun01@SYS2018DV091.home.e-kmall.com>
2018-12-18 10:25:51 +0900

    commit

--- hello/src/hello/CallMethod.java
+++ hello/src/hello/CallMethod.java
@@ -6,7 +6,10 @@ class CallMethod {
 
 	public String retrunExceptionMsg(List<String> list, String select) {
 		String returnStr = ""; // 반환할 리턴값 담을 변수
-	   	String fruitStr= ""; // 과일 값을 담을 String 변수
+	   	String listStr= "";
+
+		String connectStr = "내가 좋아하는 과일 목록은 ";
+		String connectStr2 = "입니다.";
 
 	   	String exceptionMsg="";
 		String exceptionMsg2="";
@@ -14,40 +17,84 @@ class CallMethod {
 		String exceptionMsg4="";
 
 
-	 	if(select.equals("fruit")){
+	   int lengthSize = 0;  //이름 길이 변수 초기화
+
+
+	 	if(select.equals("fruit")){ //넘겨받은 스트링 값이 fruit일 경우
 		 exceptionMsg = "과일 갯수는 최소 2개 이상이어야 합니다\n";
 		 exceptionMsg2 = "과일 갯수는 최대 10개 까지만 가능합니다\n";
 		 exceptionMsg3 = "과일 이름은 5글자 까지만 가능합니다\n";
 		 exceptionMsg4 = "과일 이름을 입력해야 합니다\n";
+
+		 lengthSize=5;
+
+			for (int i = 0; i < list.size(); i++) {
+				if (i < list.size() - 1) {
+					listStr += list.get(i) + ",";
+				} else {
+					listStr += list.get(i);
+				}
+			}
 	 	}
-		else if(select.equals("trip")){
+		else if(select.equals("trip")){ //넘겨받은 스트링 값이 trip일 경우
 		  exceptionMsg = "여행지는 최소 2개 이상이어야 합니다\n";
 		  exceptionMsg2 = "여행지는 최대 10개 까지만 가능합니다\n";
-		  exceptionMsg3 = "여행지 이름은 5글자 까지만 가능합니다\n";
+		  exceptionMsg3 = "여행지 이름은 10글자 까지만 가능합니다\n";//10
 		  exceptionMsg4 = "여행지 이름을 입력해야 합니다\n";
+
+		  lengthSize=10;
+		  connectStr ="내가 좋아하는 여행지 목록은 ";
+
+			for (int i = 0; i < list.size(); i++) {
+				if (i < list.size() - 1) {
+					listStr += list.get(i) + ",";
+				} else {
+					listStr += list.get(i);
+				}
+			}
+		}
+		else if(select.equals("food")){//넘겨받은 값이 food일 경우
+
+	 	    connectStr ="내가 좋아하는 음식 목록은 ";
+
+			exceptionMsg="음식 갯수는 최소 2개 이상이어야 한다\n";
+			exceptionMsg2="음식은 최대 10개 까지만 가능합니다.\n";
+			exceptionMsg3="음식 이름은 15글자 까지만 가능합니다.\n";
+			exceptionMsg4="음식 이름을 입력해야 합니다.\n";
+
+			 lengthSize=15;
+
+			for (int i = 0; i < list.size(); i++) {
+				if (i < list.size() - 1) {
+					listStr += list.get(i) + ",";
+				} else {
+					listStr += list.get(i);
+				}
+			}
+
 		}
 
-
-		if (list.size() < 2) {
+	/*	if (list.size() < 2) {
 			returnStr = exceptionMsg;
 		} else if (list.size() > 10) {
 			returnStr = exceptionMsg2;
-		}
+		}	else {*/
 
-		else {
-			for (int i = 0; i < list.size(); i++) {
+			 for (int i = 0; i < list.size(); i++){
 				if(list.get(i) == null){
 					returnStr = i + 1 + "번째 " + exceptionMsg4;
-
-				}
-				else if (list.get(i).trim().length() == 0) {
+				}else if (list.get(i).trim().length() == 0){
 					returnStr = i + 1 + "번째 " + exceptionMsg4;
-					System.out.println("공백체크");
-				} else if (list.get(i).length() > 5) {
+				}else if (list.get(i).length() > lengthSize) {
 					returnStr =exceptionMsg3;
-    			}
-			}
-		}
+				}else if (list.size() < 2) {
+					returnStr = exceptionMsg;
+				} else if (list.size() > 10) {
+					returnStr = exceptionMsg2;
+				}else {
+					returnStr = connectStr + listStr + connectStr2 + "\n"; // 내가 좋아하는~ 목록은 ~ 입니다.
+				}
+			 }
 		return returnStr;
    }
 }
--- hello/src/hello/CreateMethod.java
+++ hello/src/hello/CreateMethod.java
@@ -9,22 +9,24 @@ public class CreateMethod {
      	CallMethod call= new CallMethod(); //CallMethod 객체 생성
 		List<String> fruitList = new ArrayList<String>(); //과일 리스트
 		List<String> tripList = new ArrayList<String>(); // 여행 리스트
+		List<String> foodList = new ArrayList<String>(); //음식 리스트
 
 		//과일 리스트
-		fruitList.add("포도");
-	    fruitList.add("사과");
-	    fruitList.add(null);
-	    fruitList.add("수박");
-		fruitList.add("포도");
+		fruitList.add("과일");
+		fruitList.add("과일");
+		fruitList.add("과776657일");
 
 		//여행지 리스트
-		tripList.add("한국asdasd");
-		tripList.add("한국");
-		tripList.add("한국");
+		tripList.add(null);
 
-	    System.out.print(call.retrunExceptionMsg(fruitList,"fruit")); //fruit 함수 호출
-	    System.out.print(call.retrunExceptionMsg(tripList,"trip")); //fruit 함수 호출
+		//음식 리스트
+		foodList.add("음식");
+		foodList.add("음식식");
+		foodList.add("음식식식");
 
+		System.out.print(call.retrunExceptionMsg(fruitList,"fruit")); //returnException 함수 호출
+	    System.out.print(call.retrunExceptionMsg(tripList,"trip")); // returnException 함수 호출
+	    System.out.print(call.retrunExceptionMsg(foodList,"food")); //returnException fruit 함수 호출
 	}
 }
 
